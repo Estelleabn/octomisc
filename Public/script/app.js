1c64bff5ee73037e027200377b585e2f872393e2
Clement <clement.lhommeau@hotmail.fr>
2012-10-04 12:41:03 +0200

    ok

---
+++ Public/script/app.js
@@ -0,0 +1,9 @@
+function check_name(name){
+	if( name == "Gabriel"){
+		console.log("Yeah, great !");
+	}else if( name == "Clément"){
+		console.log("MAZEL TOV ! We found Clément !");
+	}else{
+		console.log("True ! " + name + " is different from Gabriel and Clément !");
+		}
+} 
